a698688e114cc549faa5303865bc8c234cb243c6
IMLaing <ianmlaing@gmail.com>
2016-06-28 22:16:12 -0400

    modified columns added social media buttons w links

--- js/script.js
+++ js/script.js
@@ -3,17 +3,17 @@ $(document).ready(function(){
 	//Fix Errors - http://www.learningjquery.com/2009/01/quick-tip-prevent-animation-queue-buildup/
 	
 	//Remove outline from links
-	$("a").click(function(){
+	$("#navButton").click(function(){
 		$(this).blur();
 	});
 	
 	//When mouse rolls over
-	$("li").mouseover(function(){
+	$("li.navLi").mouseover(function(){
 		$(this).stop().animate({height:'150px'},{queue:false, duration:600, easing: 'easeOutBounce'});
 	});
 	
 	//When mouse is removed
-	$("li").mouseout(function(){
+	$("li.navLi").mouseout(function(){
 		$(this).stop().animate({height:'50px'},{queue:false, duration:600, easing: 'easeOutBounce'});
 	});
 	
