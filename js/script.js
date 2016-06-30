a5f733ed249f9e584681ac577b68e24a24dfe277
IMLaing <ianmlaing@gmail.com>
2016-06-29 23:58:38 -0400

    added in gallery sections, added images, modified front page

--- js/script.js
+++ js/script.js
@@ -17,16 +17,42 @@ $(document).ready(function(){
 		$(this).stop().animate({height:'50px'},{queue:false, duration:600, easing: 'easeOutBounce'});
 	});
 
+	// special home
+		$("li.navHome").mouseover(function(){
+		$(this).stop().animate({height:'100px'},{queue:false, duration:600, easing: 'easeOutBounce'});
+	});
+	// special contact
+	$("li.navContact").mouseover(function(){
+	$(this).stop().animate({height:'150px',width:'150px'},{queue:false, duration:600, easing: 'easeOutBounce'});
+	});
+	$("li.navContact").mouseout(function(){
+		$(this).stop().animate({height:'50px',width:'100px'},{queue:false, duration:600, easing: 'easeOutBounce'});
+	});
+
 // init Masonry
 var $grid = $('.grid').masonry({
   itemSelector: '.grid-item',
-  percentPosition: true,
-  columnWidth: '.grid-sizer'
-	});
+  columnWidth: '.grid-sizer',
+  percentPosition: true
+});
 
 // init imagesloaded
 $grid.imagesLoaded().progress( function() {
 	$grid.masonry('layout');
 	});
 
+});
+
+//  Scroll to Top  
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
 });
