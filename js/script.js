173375e3f3d03594d0609887a93786b142cc50dc
IMLaing <ianmlaing@gmail.com>
2016-07-30 12:49:39 -0400

    added return to top on gallery

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
-$(document).ready(function(){
+$(function(){
 	
-	//Fix Errors - http://www.learningjquery.com/2009/01/quick-tip-prevent-animation-queue-buildup/
-	
+//  Scroll to Top  
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
 	//Remove outline from links
 	$("#navButton").click(function(){
 		$(this).blur();
@@ -43,16 +54,3 @@ $grid.imagesLoaded().progress( function() {
 
 });
 
-//  Scroll to Top  
-$(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-        $('#return-to-top').fadeIn(200);    // Fade in the arrow
-    } else {
-        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
-    }
-});
-$('#return-to-top').click(function() {      // When arrow is clicked
-    $('body,html').animate({
-        scrollTop : 0                       // Scroll to top of body
-    }, 500);
-});
